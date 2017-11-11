44e796a2237a0fb221d293388625692ec319b99e
Joshua Rambert <JoshuaRambert@Joshuas-MacBook-Pro.local>
2017-11-11 12:55:45 -0800

    Another first commit

---
+++ FinalProject/FinalProject/MainPageViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MainPageViewController.swift
+//  FinalProject
+//
+//  Created by Joshua Rambert  on 11/11/17.
+//  Copyright © 2017 Joshua Rambert . All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainPageViewController: UIViewController, UITableViewController {
+    
+}

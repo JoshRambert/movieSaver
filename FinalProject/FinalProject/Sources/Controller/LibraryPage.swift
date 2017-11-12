3598d8e9ae94d9cf777ab65629e4466c15621350
Joshua Rambert <JoshuaRambert@Joshuas-MacBook-Pro.local>
2017-11-12 02:54:58 -0800

    another commit

---
+++ FinalProject/FinalProject/Sources/Controller/LibraryPage.swift
@@ -0,0 +1,14 @@
+//
+//  LibraryPage.swift
+//  FinalProject
+//
+//  Created by Joshua Rambert  on 11/12/17.
+//  Copyright © 2017 Joshua Rambert . All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LibraryPageViewController: UIViewController {
+    
+}
---
+++ FinalProject/FinalProject/Sources/Controller/WhatsNew.swift
@@ -0,0 +1,14 @@
+//
+//  WhatsNew.swift
+//  FinalProject
+//
+//  Created by Joshua Rambert  on 11/12/17.
+//  Copyright © 2017 Joshua Rambert . All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class WhatsNewViewController: UIViewController {
+    
+}

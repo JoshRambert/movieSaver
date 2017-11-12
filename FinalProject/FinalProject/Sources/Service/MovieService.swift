44438d0a5a0e204c1c7f2841fe59c13e328e6e8b
Joshua Rambert <JoshuaRambert@Joshuas-MacBook-Pro.local>
2017-11-11 23:42:20 -0800

    Another commit

---
+++ FinalProject/FinalProject/Sources/Service/MovieService.swift
@@ -0,0 +1,14 @@
+//
+//  MovieService.swift
+//  FinalProject
+//
+//  Created by Joshua Rambert  on 11/11/17.
+//  Copyright © 2017 Joshua Rambert . All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MovieService {
+    
+}

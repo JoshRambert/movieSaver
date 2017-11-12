c84526c84e286e5fd80b0415fbdaa25fafe3f115
Joshua Rambert <JoshuaRambert@Joshuas-MacBook-Pro.local>
2017-11-12 15:29:38 -0800

    added to the MainPageViewController class

--- FinalProject/FinalProject/Sources/Controller/MainPageViewController.swift
+++ FinalProject/FinalProject/Sources/Controller/MainPageViewController.swift
@@ -9,6 +9,32 @@
 import Foundation
 import UIKit
 
-class MainPageViewController: UIViewController, UITableViewController {
+class MainPageViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    //Setup the functions for the TableView
+    func tableView(_ tableView: UITableView, numberOfRowsInSection: Int) -> Int {
+        //return the Amount of Data from the MovieService Class
+    }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //Use this Function to display the data onto the table View
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
+        
+        //access the values from the MovieService class that configured yet
+        return cell
+    }
+    
+    //use a prepare for sugue to send the data from the tableView into the Description page
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "DescriptionSegue" {
+            let descriptionPageViewController = segue.destination as! DescriptionPage
+            
+            //send te data over to the description page -- and send the data back when it is deselected
+        }
+    }
+    //Create an outlet for the Table in the ViewController
+    @IBOutlet private weak var movieListTable: UITableView!
 }

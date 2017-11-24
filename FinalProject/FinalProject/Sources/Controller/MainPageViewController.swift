5fa0c775830a3704b72564fd23dd83bae531d951
Joshua Rambert <JoshuaRambert@Joshuas-MacBook-Pro.local>
2017-11-24 04:09:49 -0800

    not sending over the correct data from every cell

--- FinalProject/FinalProject/Sources/Controller/MainPageViewController.swift
+++ FinalProject/FinalProject/Sources/Controller/MainPageViewController.swift
@@ -21,52 +21,16 @@ class MainPageViewController: UIViewController, UITableViewDataSource, UITableVi
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        //Use this Function to display the data onto the table View
         let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
-        //access the values from the MovieService class that configured yet
         let horrorValues = MovieService.shared.horrorMovieData()[indexPath.row]
-        let actionValues = MovieService.shared.actionMovieData()[indexPath.row]
-        let comedyValues = MovieService.shared.comedyMovieData()[indexPath.row]
-        //let sportsValues = MovieService.shared.sportsMovieData()[indexPath.row]
         
-        //Whenever the butotn is clicked that corresponds with the gnere of the movie change the contents of the
-        //Cell view
-        if horrorButton.isTouchInside {
-            let titleName = horrorValues.title
-            let imageName = horrorValues.image
-            let description = horrorValues.description
-            cell.getImage(forImageName: imageName)
-            cell.getTitle(forTitleName: titleName)
-            cell.getDescription(forDescription: description)
+        let titleName = horrorValues.title
+        let imageName = horrorValues.image
+        let description = horrorValues.description
+        cell.getImage(forImageName: imageName)
+        cell.getTitle(forTitleName: titleName)
+        cell.getDescription(forDescription: description)
             
-        } else if actionButton.isTouchInside {
-            let titleName = actionValues.title
-            let description = actionValues.description
-            let imageName = actionValues.image
-            cell.getImage(forImageName: imageName)
-            cell.getTitle(forTitleName: titleName)
-            cell.getDescription(forDescription: description)
-            
-        } else if comedyButton.isTouchInside {
-            let titleName = comedyValues.title
-            let description = comedyValues.description
-            let imageName = comedyValues.image
-            cell.getTitle(forTitleName: titleName)
-            cell.getDescription(forDescription: description)
-            cell.getImage(forImageName: imageName)
-            
-        } else if sportsButton.isTouchInside {
-           /* let titleName = sportsValues.title
-            let description = sportsValues.description
-            let imageName = sportsValues.image
-            cell.getImage(forImageName: imageName)
-            cell.getTitle(forTitleName: titleName)
-            cell.getDescription(forDescription: description)
-            */
-        }
-        //MARK: Add a gesture that when the movieCell is held down it will bring up an alert view that will give the user the option to save the contents of that movieCell
-        
-        
         return cell
     }
     
@@ -84,12 +48,59 @@ class MainPageViewController: UIViewController, UITableViewDataSource, UITableVi
         }
     }
     
-    //Create an outlet for the Table in the ViewController
+    //MARK: Outlets and Properties
     @IBOutlet private weak var movieListTable: UITableView!
+    private var genreIndex: IndexPath! = nil
     
-    //When each button is clicked it should change the contents of the MovieCell
-    @IBOutlet private weak var horrorButton: UIButton!
-    @IBOutlet private weak var actionButton: UIButton!
-    @IBOutlet private weak var comedyButton: UIButton!
-    @IBOutlet private weak var sportsButton: UIButton!
+    //Create a Function for each of the Buttons and change the contents of the cell whenever the button is clicked
+    @IBAction private func horrorButton() {
+        //Change the contents that is within the tableView Whenever this button is clicked -- first acess the
+        //contents of the cell
+        let cell = movieListTable.dequeueReusableCell(withIdentifier: "MovieCell", for: genreIndex) as! MovieCell
+        let horrorValues = MovieService.shared.horrorMovieData()[genreIndex.row]
+        
+        let titleName = horrorValues.title
+        let imageName = horrorValues.image
+        let description = horrorValues.description
+        cell.getImage(forImageName: imageName)
+        cell.getTitle(forTitleName: titleName)
+        cell.getDescription(forDescription: description)
+    }
+    
+    @IBAction private func comedyButton() {
+        //do the same for this actoin button that you did for th one before
+        let cell = movieListTable.dequeueReusableCell(withIdentifier: "MovieCell", for: genreIndex) as! MovieCell
+        let comedyValues = MovieService.shared.comedyMovieData()[genreIndex.row]
+        
+        let titleName = comedyValues.title
+        let imageName = comedyValues.image
+        let description = comedyValues.description
+        cell.getImage(forImageName: imageName)
+        cell.getTitle(forTitleName: titleName)
+        cell.getDescription(forDescription: description)
+    }
+    
+    @IBAction private func actionButton() {
+        let cell = movieListTable.dequeueReusableCell(withIdentifier: "MovieCell", for: genreIndex) as! MovieCell
+        let actionValues = MovieService.shared.actionMovieData()[genreIndex.row]
+        
+        let titleName = actionValues.title
+        let imageName = actionValues.image
+        let description = actionValues.description
+        cell.getImage(forImageName: imageName)
+        cell.getTitle(forTitleName: titleName)
+        cell.getDescription(forDescription: description)
+    }
+    
+    @IBAction private func sportsButton() {
+        let cell = movieListTable.dequeueReusableCell(withIdentifier: "MovieCell", for: genreIndex) as! MovieCell
+        let sportsValues = MovieService.shared.sportsMovieData()[genreIndex.row]
+        
+        let titleName = sportsValues.title
+        let imageName = sportsValues.image
+        let description = sportsValues.description
+        cell.getImage(forImageName: imageName)
+        cell.getTitle(forTitleName: titleName)
+        cell.getDescription(forDescription: description)
+    }
 }
